0dc647901f429763a554f492182e399a784cdf75
Black-NET <63128435+Black-NET@users.noreply.github.com>
2020-10-05 23:03:12 +0300

    Update blacklist.cpp

--- src/blacklist/blacklist.cpp
+++ src/blacklist/blacklist.cpp
@@ -6,17 +6,17 @@
 std::vector<std::string> blacklistedAddrs {
     //"XYe8FWCx6cdbN937xEuWxTJoFJCp9Zkpzz",
     //"XXx8Vc6R1zNKif2PMrTD67tTyvxnFwyrQq",
-	//"XKSzmXVNbBPGNmhMZN69iwnQzqnwn6tsWG",
-	//"XN9Rkq2ynVrudCVtiN5y8c7eKckzVmh3Zo",
-	//"XHT8mbEtyYjudYE5cYAebEDonEKNy2JT6R",
-	//"XTuQ14aW64ZmbntsiMvzas1DJenvSuQEtV",
-	//"XCFGzEdAadCxrGWgRjU4t5JGbX8SaeCjtn",
-	//"XBvQq5XRXmgR11zhLkTR5V3UpSaYq8uAnW",
-	//"XXYKXBydQ6SvC62nMqTAxFDMst36PWv4pr",
-	//"XByhXWTwnCh6sqzoZ1S8snygaAky4P4CLP",
-	//"XWJEAiNKCad68ZV8oziVDWDun32NFsnoMZ",
-	//"XYKvhXzxFrSyiqADL5xnBkhTPKCRNG9PxC",
-	//"XRZL66HDWyGJ3QsurGcfykQuUyYBxNBBuz",
+    //"XKSzmXVNbBPGNmhMZN69iwnQzqnwn6tsWG",
+    //"XN9Rkq2ynVrudCVtiN5y8c7eKckzVmh3Zo",
+    //"XHT8mbEtyYjudYE5cYAebEDonEKNy2JT6R",
+    //"XTuQ14aW64ZmbntsiMvzas1DJenvSuQEtV",
+    //"XCFGzEdAadCxrGWgRjU4t5JGbX8SaeCjtn",
+    //"XBvQq5XRXmgR11zhLkTR5V3UpSaYq8uAnW",
+    //"XXYKXBydQ6SvC62nMqTAxFDMst36PWv4pr",
+    //"XByhXWTwnCh6sqzoZ1S8snygaAky4P4CLP",
+    //"XWJEAiNKCad68ZV8oziVDWDun32NFsnoMZ",
+    //"XYKvhXzxFrSyiqADL5xnBkhTPKCRNG9PxC",
+    //"XRZL66HDWyGJ3QsurGcfykQuUyYBxNBBuz",
     //"XBvfaAah3KTzXND6vVhKRERUydNirsZzkC"
 };
 bool ContainsBlacklistedAddr(std::string addr){
